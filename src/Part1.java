9d29c4e0c2807a8caf60a63aa5df97fee0d9642a
vitko5 <cp.advert0@googlemail.com>
2022-07-08 14:42:43 +0300

    Initial commit

---
+++ src/Part1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Part1 {
+    // Користувач вводить слова з консолі, в масив додавати лише ті, що починаються на "а"
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String stringInput = sc.nextLine();
+
+    }
+}
